7a062699a56592eaee8b5fc68cb4d9f9d1d5dab4
Justin-Wang-ICD2O1-01 <Justin.wang24@student.tdsb.on.ca>
2024-11-05 13:45:41 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -3,16 +3,18 @@
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-
+print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n* * * * *\n*       *\n*       *\n*       *\n* * * * *\n* * * * * *\n *    *\n  *   *\n   *  *\n    * *\n     *", end = "")
 def q2():
   #Write Assignment code here
-  
+  print("Hello World", end = "")
 def q3():
   #Write Assignment code here
+  print("Man"\n"Board"\n"Man Overboard", end = "")
+  
   
 
 
 #Do not alter anything after this line
-q1()
-q2()
-q3()
+#q1()
+#q2()
+#q3()
